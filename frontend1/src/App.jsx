346ee6d9c96996a3508ea417a1b6c7038667e66c
Sagar Seth <lightningsagar0@gmail.com>
2024-11-14 19:29:23 +0530

    added the puzzle6

--- frontend1/src/App.jsx
+++ frontend1/src/App.jsx
@@ -9,6 +9,7 @@ import PuzzleJourney from "./Pages/Puzzlejourney.jsx";
 import Puzzle1 from "./Pages/Puzzle1.jsx";
 import Puzzle2 from "./Pages/Puzzle2.jsx";
 import Puzzle4 from "./Pages/Puzzle4/Puzzle4.jsx";
+import Puzzle6 from "./Pages/Puzzle6/Puzzle6.jsx";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Route path="/puzzle/1" element={<Puzzle1 />} />
         <Route path="/puzzle/2" element={<Puzzle2 />} />
         <Route path="/puzzle/4" element={<Puzzle4 />} />
+        <Route path="/puzzle/6" element={<Puzzle6 />} />
       </Routes>
     </div>
   );
