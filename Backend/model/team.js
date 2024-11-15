66de4c7b739a6efa534288d3c3b36f8dfe3df144
Vansh Verma <vvansh739@gmail.com>
2024-11-15 15:44:52 +0530

    pushing more changes

--- Backend/model/team.js
+++ Backend/model/team.js
@@ -4,20 +4,25 @@ import { Schema } from "mongoose";
 const teamSchema = Schema({
     leader_email:{
         type:String,
-        required:true
+        required:true,
+        unique: true, 
     },
     password:{
         type:String,
-        required:true
+        required:true,
+   
     },
     member1:{
         type:String,
+        unique:true
     },
     member2:{
         type:String,
+        unique:true
     },
     member3:{
         type:String,
+        unique:true
     },
 }) 
 
